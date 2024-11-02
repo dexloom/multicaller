aba0151904774099bf1db8a56f5b5d052c64bb23
cakevm <cakevm@proton.me>
2024-11-02 08:49:30 +0100

    Lint & fix rebase

--- test/Helper.sol
+++ test/Helper.sol
@@ -21,44 +21,43 @@ address constant MULTICALLER = address(0x787878787878787878787878787878787878787
 address constant WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 address constant USDT = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 
-abstract contract SwapTest is Test{
-    function get_call_data(uint256 i ) virtual internal returns (bytes memory);
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {ERC20} from "./mocks/ERC20.sol";
 
-    function get_test_name(uint256 i ) virtual internal returns (string memory);
+abstract contract SwapTest is Test {
+    function get_call_data(uint256 i) internal virtual returns (bytes memory);
 
-    function get_count() virtual internal returns (uint256);
+    function get_test_name(uint256 i) internal virtual returns (string memory);
 
-    function get_swap_token() virtual internal returns (address);
+    function get_count() internal virtual returns (uint256);
 
-    function get_multicaller() virtual internal returns (address);
+    function get_swap_token() internal virtual returns (address);
 
+    function get_multicaller() internal virtual returns (address);
 
-    function test_all( ) public {
-
+    function run_test_all() public {
         uint256 snapshot = vm.snapshot();
 
-        for( uint256 i = 0 ; i < get_count(); i++) {
-            test_one(i);
+        for (uint256 i = 0; i < get_count(); i++) {
+            run_test_one(i);
             vm.revertTo(snapshot);
         }
-
     }
 
-    function test_one(uint256 index) public {
+    function run_test_one(uint256 index) public {
         uint256 gasLeft = gasleft();
         uint256 balanceBefore = ERC20(get_swap_token()).balanceOf(get_multicaller());
-        (bool result, ) = get_multicaller().call( get_call_data(index) );
+        (bool result, ) = get_multicaller().call(get_call_data(index));
         uint256 gasUsed = gasLeft - gasleft();
         uint256 balanceAfter = ERC20(get_swap_token()).balanceOf(get_multicaller());
         console.log(index, result, get_test_name(index), balanceAfter);
-        if(  balanceBefore == balanceAfter || balanceAfter > balanceBefore || balanceBefore - balanceAfter > 0.001 ether) {
-            console.log(index,"failed");
+        if (balanceBefore == balanceAfter || balanceAfter > balanceBefore || balanceBefore - balanceAfter > 0.001 ether) {
+            console.log(index, "failed");
         }
         assertEq(result, true);
     }
-
 }
-
 
 contract TestHelper is Test {
     MultiCaller public multicaller;
@@ -110,9 +109,6 @@ contract TestHelper is Test {
         vm.startPrank(0xF977814e90dA44bFA03b6295A0616a897441aceC); // Binance wallet for funding
         usdt.transfer(address(multicaller), 1_000_000_000); // 1k
     }
-
-    
-
 }
 
 contract Helper {
